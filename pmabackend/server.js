561269da00a57c9cc779f9875eeeaf4bd0c255d4
Sudhanshu10Sathawane <sudhanshusathwane@gmail.com>
2024-01-12 09:55:10 +0530

    Error handler added

--- pmabackend/server.js
+++ pmabackend/server.js
@@ -3,7 +3,9 @@ const dotenv=require("dotenv").config();
 const app=express();
 const port=process.env.PORT || 5000;
 const connectDb = require("./Config/dbConnection");
+const errorHandler = require("./Middleware/ErrorHandler");
 connectDb();
+app.use(errorHandler)
 app.listen(port, ()=>{
     console.log('Server Running on port',port);
 });
